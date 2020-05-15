17e89478e1c6cf918e407faa96cfade3dd36718e
문영진 <dudwls0113@gmail.com>
2020-05-16 03:45:44 +0900

    Clova CSR SDK 연동, 음성인식을 처리하는 ClovaViewModel 구현

--- app/src/main/java/com/naver/hackday/android/speechrecognizecalender/src/common/util/AppConstants.java
+++ app/src/main/java/com/naver/hackday/android/speechrecognizecalender/src/common/util/AppConstants.java
@@ -30,6 +30,9 @@ public final class AppConstants {
     public static final SimpleDateFormat DATE_FORMAT_KR = new SimpleDateFormat("yyyy년 MM월 dd일", Locale.KOREA);
     public static final SimpleDateFormat DATE_FORMAT_EN = new SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH);
 
+    //Naver Clova API
+    public static final String CLIENT_ID = "tcc3ojy059";
+
     // Retrofit 인스턴스
     public static Retrofit retrofit;
 
--- app/src/main/java/com/naver/hackday/android/speechrecognizecalender/src/ui/event/MonthlyEventsActivity.java
+++ app/src/main/java/com/naver/hackday/android/speechrecognizecalender/src/ui/event/MonthlyEventsActivity.java
@@ -1,9 +1,12 @@
 package com.naver.hackday.android.speechrecognizecalender.src.ui.event;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -11,6 +14,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 import com.naver.hackday.android.speechrecognizecalender.R;
 import com.naver.hackday.android.speechrecognizecalender.databinding.ActivityMontlyCalanderBinding;
 import com.naver.hackday.android.speechrecognizecalender.src.BaseActivity;
+import com.naver.hackday.android.speechrecognizecalender.src.network.clova.ClovaViewModel;
 import com.naver.hackday.android.speechrecognizecalender.src.ui.event.adapters.MonthlyFragmentAdapter;
 import com.naver.hackday.android.speechrecognizecalender.src.ui.event.fragments.MonthlyCalenderFragment;
 import com.naver.hackday.android.speechrecognizecalender.src.ui.event.viewModels.EventViewModel;
@@ -21,6 +25,7 @@ import java.util.List;
 public class MonthlyEventsActivity extends BaseActivity {
 
     private EventViewModel mEventViewModel;
+    private ClovaViewModel mClovaViewModel;
     private ActivityMontlyCalanderBinding mBinding;
     private MonthlyFragmentAdapter mMonthlyFragmentAdapter;
     private List<String> mTabTitleArray = new ArrayList<>();
@@ -35,12 +40,13 @@ public class MonthlyEventsActivity extends BaseActivity {
         /* ViewModel & Binding */
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_montly_calander);
         mEventViewModel = new ViewModelProvider(getViewModelStore(), viewModelFactory).get(EventViewModel.class);
+        mClovaViewModel = new ViewModelProvider(getViewModelStore(), viewModelFactory).get(ClovaViewModel.class);
         mBinding.setEventViewModel(mEventViewModel);
+        mBinding.setClovaViewModel(mClovaViewModel);
 
         /* ViewPager */
         mMonthlyFragmentAdapter = new MonthlyFragmentAdapter(getSupportFragmentManager(), getLifecycle());
         mBinding.activityMonthlyCalenderVp.setAdapter(mMonthlyFragmentAdapter);
-
     }
 
     @Override
@@ -64,7 +70,15 @@ public class MonthlyEventsActivity extends BaseActivity {
 
     @Override
     protected void initAfterBinding() {
+        permissionCheck();
+    }
 
+    public void permissionCheck(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
     }
 
 }
